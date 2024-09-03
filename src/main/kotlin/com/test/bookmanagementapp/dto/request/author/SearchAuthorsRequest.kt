a216e64a238fdd976c971342c6706244da7c3ef4
imk0512 <imk3626@gmail.com>
2024-09-03 23:07:30 +0900

    fix: remove Nationality （見落とし分)

--- src/main/kotlin/com/test/bookmanagementapp/dto/request/author/SearchAuthorsRequest.kt
+++ src/main/kotlin/com/test/bookmanagementapp/dto/request/author/SearchAuthorsRequest.kt
@@ -1,7 +1,11 @@
 package com.test.bookmanagementapp.dto.request.author
 
+import io.swagger.v3.oas.annotations.media.Schema
+
 data class SearchAuthorsRequest(
+    @Schema(description = "Name of the author", example = "夏目漱石")
     val name: String? = null,
-    val nationality: String? = null,
+
+    @Schema(description = "Birthdate of the author", example = "1867-02-09")
     val birthdate: String? = null
 )
